dc5ae472918121408674c35c68fb9d8e5c1ef1b7
WolfyVST <60368728+WolfyVST@users.noreply.github.com>
2021-03-03 12:34:13 -0500

    Problem 2 of the hwk1

---
+++ hw01/problem2.java
@@ -0,0 +1,12 @@
+public class hwk1problem2 {
+    public static void main(String[] args) {
+        double V0 = 5.6;
+        double V1 = 10.5;
+        double t = 0.5;
+        double a ;
+
+        a = (V1 - V0) / t ;
+
+        System.out.println("The avarage acceleration is " + a);
+    }
+}
