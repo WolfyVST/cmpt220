bb9b2a880bc23d23cdb5aaae335b9717c2e4d01f
WolfyVST <60368728+WolfyVST@users.noreply.github.com>
2021-03-03 12:25:32 -0500

    Problem 1 of the hwk

---
+++ hw01/Problem1.java
@@ -0,0 +1,14 @@
+public class hwk1problem1 {
+    public static void main(String[] args) {
+        double discriminat ;
+        int a = 3 ;
+        int b = 4 ;
+        int c = 5 ;
+
+        discriminat = Math.pow(b,2) - (4 * a * c);
+
+        System.out.println("The discriminant for the equation 3x^2 + 4x + 5 = 0 is " + discriminat);
+
+
+    }
+}
